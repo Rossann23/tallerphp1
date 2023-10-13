cd2a1c5c69eff2114258a9fb0ec5d9942b3fc48c
Rossann23 <anassor2004@gmail.com>
2023-10-13 12:46:30 -0300

    tallerphp01

---
+++ index1.php
@@ -0,0 +1,11 @@
+<?php
+    $cadena1 = "Hola";
+    $cadena2 = "Mundo";
+    echo $cadena1." ".$cadena2."<br>"; 
+    echo "$cadena1 $cadena2<br>"; 
+
+    $dia = 11;
+    $fecha = "Hoy es $dia de octubre";
+    echo $fecha;
+
+?>
